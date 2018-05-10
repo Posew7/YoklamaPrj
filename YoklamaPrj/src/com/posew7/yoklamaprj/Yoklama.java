e9000bf1d2115fa6a62b4de6dc752ac0c0e096b6
Yunus <Yunus@10.42.132.63>
2018-05-10 05:21:42 +0300

    $##

---
+++ YoklamaPrj/src/com/posew7/yoklamaprj/Yoklama.java
@@ -0,0 +1,14 @@
+package com.posew7.yoklamaprj;
+
+public class Yoklama {
+
+	public static void main(String[] args) {
+		
+		String isimler[] = {"yunus","gülþah","bora","taner","boðaç","berna","sertaç"};
+		
+		for (int i = 0; i < isimler.length; i++) {
+			System.out.println(isimler[i]);
+		}
+	}
+
+}
